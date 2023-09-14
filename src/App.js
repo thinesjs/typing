699ec55942b7562b07a42d8cafa6a406c12210b4
thinesjs <thinesjs@gmail.com>
2023-09-15 00:55:43 +0800

    setup project files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Footer from './layouts/Footer';
+import Home from './pages/Home';
+import Navbar from './layouts/Navbar';
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      }
+    ]
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="app" >
+          <RouterProvider router={router} />
+      </div>
+    </ChakraProvider>
   );
 }
 
---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@chakra-ui/react'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div>Navbar</div>
+    <Button colorScheme='blue'>Button</Button>
+    </>
+  )
+}
+
+export default Navbar
