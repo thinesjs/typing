85bbc36b29255873cfcc1f48cb7d2e0c27020a6a
thinesjs <thinesshankar@moneymatch.co>
2025-08-17 15:36:18 +0800

    fix: updated domain to current for safety purposes

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,11 +1,10 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
-import React from 'react'
 
 const Footer = () => {
   return (
     <Box p="4">
       <Flex justifyContent="center" alignItems="center">
-        <Text>&copy; 2023 thinesjaishankar.com/type</Text>
+        <Text>&copy; 2023 thinesjs.com/type</Text>
       </Flex>
     </Box>
   )
